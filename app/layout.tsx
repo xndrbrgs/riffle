cb72cb42b2e2e20d3c962324c0867c9ce49ea6db
xndrbrgs <aborgescolon@gmail.com>
2023-10-21 16:06:16 -0400

    added prisma as backend + userAuth

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import { ThemeProvider } from "@/components/providers/theme-provider";
+import { cn } from "@/lib/utils";
 
 const font = Open_Sans({ subsets: ["latin"] });
 
@@ -18,8 +19,9 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className,
+          'bg-[#F4F0E6] dark:bg-[#1A1A1A]')}>
           <ThemeProvider
             attribute="class"
             defaultTheme="dark"
