bce1e8dff71002cdfaed6c357d21344b63542b92
Alex Borges <aborgescolon@gmail.com>
2023-10-22 16:15:57 -0400

    invites need confirmation

--- components/modals/invite-modal.tsx
+++ components/modals/invite-modal.tsx
@@ -8,15 +8,18 @@ import {
 } from "@/components/ui/dialog";
 
 import { useModal } from "@/hooks/use-modal-store";
+
 import { Label } from "../ui/label";
 import { Input } from "../ui/input";
 import { Button } from "../ui/button";
 import { Check, Copy, RefreshCw } from "lucide-react";
+
 import { useOrigin } from "@/hooks/use-origin";
 import { useState } from "react";
+import axios from "axios";
 
 export const InviteModal = () => {
-  const { isOpen, onClose, type, data } = useModal();
+  const { onOpen, isOpen, onClose, type, data } = useModal();
   const origin = useOrigin();
 
   const isModalOpen = isOpen && type === "invite";
@@ -36,6 +39,21 @@ export const InviteModal = () => {
     }, 1000);
   };
 
+  const onNew = async () => {
+    try {
+      setIsLoading(true);
+      const response = await axios.patch(
+        `/api/servers/${server?.id}/invite-code`
+      );
+
+      onOpen("invite", { server: response.data });
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <Dialog open={isModalOpen} onOpenChange={onClose}>
       <DialogContent className="bg-white text-black p-0 overflow-hidden">
@@ -50,10 +68,11 @@ export const InviteModal = () => {
           </Label>
           <div className="flex items-center mt-2 gap-x-2">
             <Input
+              disabled={isLoading}
               className="bg-zinc-300/50 border-0 focus-visible:ring-0 focus-visible:ring-offset-0 text-black"
               value={inviteUrl}
             />
-            <Button onClick={onCopy} size="icon">
+            <Button disabled={isLoading} onClick={onCopy} size="icon">
               {copied ? (
                 <Check className="w-4 h-4" />
               ) : (
@@ -62,6 +81,8 @@ export const InviteModal = () => {
             </Button>
           </div>
           <Button
+            onClick={onNew}
+            disabled={isLoading}
             size="sm"
             className="text-xs text-zinc-500 mt-4"
             variant="link"
