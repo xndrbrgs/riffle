ac91b396b8c7fd8b62a01381704391310b3a6c5b
xndrbrgs <aborgescolon@gmail.com>
2023-10-25 17:23:39 -0400

    burger menu working + schema updated

---
+++ components/chat/chat-header.tsx
@@ -0,0 +1,24 @@
+import { Hash } from "lucide-react";
+import { MobileToggle } from "../mobile-toggle";
+
+interface ChatHeaderProps {
+  serverId: string;
+  name?: string;
+  type: "channel" | "conversation";
+  imageUrl?: string;
+}
+
+const ChatHeader = ({ serverId, name, type, imageUrl }: ChatHeaderProps) => {
+  return (
+    <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
+      <MobileToggle serverId={serverId} />
+
+      {type === "channel" && (
+        <Hash className="h-5 w-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+      )}
+      <p className="text-md font-semibold text-black dark:text-white">{name}</p>
+    </div>
+  );
+};
+
+export default ChatHeader;
